0733187921e78b859c27389e4a7d94ba90989fd6
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-04-05 22:13:58 +0100

    added folders

---
+++ LOOPS/script.js
@@ -0,0 +1,40 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy",
+];  
+//var todosLength =todos.length;
+//for (var i=0; i <  todos.length; i++) {
+  //  console.log(i);
+//}
+
+//todos.forEach(function(todos, i){
+  //  console.log(todos, i);
+//})
+
+function logTodos(todo, i) {
+    console.log(todo, i);
+
+}
+ todos.forEach(logTodos);
+
+
+//var counterOne = 0;
+//while (counterOne < 10){
+   // console.log(counterOne);
+    //counterOne++
+//}
+
+//var counterOne = 10;
+//while (counterOne > 0){
+  //  console.log(counterOne);
+    //counterOne--;
+//}
+ 
+//var counterTwo = 10
+//do {
+  //  console.log(counterTwo);
+    //counterTwo--;
+//} while (counterTwo > 0);
